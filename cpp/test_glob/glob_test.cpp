a4e2e119f80c74c205008cbc656728f3598e3e19
pingheng <pingheng@corp.netease.com>
2020-01-15 21:42:03 +0800

    glob

---
+++ cpp/test_glob/glob_test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "glob.h"
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " pattern1 [pattern2 [...]]"
+                            << std::endl;
+        return 1;
+    }
+    for (int i = 1; i < argc; i++) {
+        glob::glob glob(argv[i]);
+        while (glob) {
+            std::cout << glob.current_match() << std::endl;
+            glob.next();
+        }
+    }
+    return 0;
+}
